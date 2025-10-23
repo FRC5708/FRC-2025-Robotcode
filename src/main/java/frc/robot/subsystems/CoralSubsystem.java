bd220c7bdaf381a6cf8c719e06a21ffae8b90376
Lincoln <lincoln@vulpes.com>
2025-10-22 20:10:40 -0400

    ah

--- src/main/java/frc/robot/subsystems/CoralSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralSubsystem.java
@@ -1,7 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
+ //"absoluteEncoderOffset": 300.05856,
 package frc.robot.subsystems;
 
 import frc.robot.Constants;
